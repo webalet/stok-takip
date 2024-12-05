b92654d22e242ec7eb0e1eae79b8c431e3d1e8ea
Your Name <you@example.com>
2024-12-06 00:23:02 +0300

    lk yükleme - Tüm proje dosyaları

---
+++ config.py
@@ -0,0 +1,5 @@
+SIFRE = "010901"
+SECRET_KEY = 'gizli_anahtar_123'
+UPLOAD_FOLDER = 'static/uploads'
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'} 
